d87a56b44b3a88cf9d8dfd620f5c806eccc965ae
d_polisensky <d_polisensky@utb.cz>
2024-12-14 11:00:11 +0100

    create setting page

--- src/app/api/schedule.service.ts
+++ src/app/api/schedule.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { ScheduleResponse } from '../models/schedule-response';
 import { StudentSubjectDetailResponse } from '../models/subject-response';
-
+import { Storage } from '@ionic/storage-angular';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,13 +12,15 @@ export class ScheduleService {
   private corsProxyUrl = 'https://cors-anywhere.herokuapp.com/';
   private apiUrl = 'https://stag-ws.utb.cz/ws/services/rest2/';
 
-  constructor(private http: HttpClient ) {}
+
+  constructor(private http: HttpClient, private storage: Storage ) {}
+
 
   getSchedule(osCislo:string, semestr:string, rok:string ): Observable<ScheduleResponse> {
     const url = `${this.corsProxyUrl}${this.apiUrl}rozvrhy/getRozvrhByStudent??stagUser=${osCislo}&semestr=${semestr}&jenRozvrhoveAkce=true&lang=cs&outputFormat=JSON&osCislo=${osCislo}&rok=${rok}`;
- 
+
     const headers = new HttpHeaders({
-      'Authorization': 'Basic ' + btoa(`${username}:${password}`),
+      'Authorization': 'Basic ' + btoa(`${this.username}:${this.password}`),
       'X-Requested-With': 'XMLHttpRequest'
     });
 
@@ -36,7 +38,9 @@ export class ScheduleService {
   }
   getsubjects(osCislo:string, semestr:string, rok:string ): Observable<StudentSubjectDetailResponse> {
     const url = `${this.corsProxyUrl}${this.apiUrl}znamky/getZnamkyByStudent?stagUser=${osCislo}&semestr=${semestr}&outputFormat=JSON&osCislo=${osCislo}&rok=${rok}`;
-   
+    const username = "d_polisensky";
+    const password = "Dreft15dreft15";
+  
     const headers = new HttpHeaders({
       'Authorization': 'Basic ' + btoa(`${username}:${password}`),
       'X-Requested-With': 'XMLHttpRequest'
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+ 
 
 @Component({
   selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  templateUrl: './tab3.page.html',
+  styleUrls: ['./tab3.page.scss'],
 })
 export class Tab3Page {
+  username: string = '';
+  password: string = '';;
 
-  constructor() {}
+  constructor(private navCtrl: NavController, private storage: Storage) {
+    this.storage.create();
+  }
 
+  async saveCredentials() {
+    await this.storage.set('username', this.username);
+    await this.storage.set('password', this.password);
+    console.log('Credentials saved');
+    this.navCtrl.back();
+  }
+
+  cancel() {
+    // Navigate back or reset the form
+    this.navCtrl.back();
+  }
 }
